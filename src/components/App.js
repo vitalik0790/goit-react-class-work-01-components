b59e93412ba9d55acedf70a59147df2b1334a216
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-02-24 20:00:07 +0200

    last

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// slr  стрелка
+// rfc  так як Андрій робив
+// rafc  стрелка
+const App = () => {
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+};
+
+export default App;
